f51886982ef5958a629f1cc38b51a535cb0f6772
小池武司 <harknesslancer@gmail.com>
2019-03-28 20:59:37 +0900

    lesson1

---
+++ lesson1.py
@@ -0,0 +1,8 @@
+name = 'stressed'
+neme2 = list(reversed(name))
+name3 = ''.join(list(reversed(name)))
+print(name3)
+
+name = 'パクトクカシーー'
+name1 = name[0] + name[2] + name[4] + name[6]
+print(name1)
